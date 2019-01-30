0dff68876c30f3c83c8dc45e05dd52d0e9b7910a
wonuk <wonuk007@naver.com>
2019-01-30 13:18:53 +0900

    비밀번호 수정 사용자컨트롤

---
+++ GoodeeProject/CtlModifyPW.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace GoodeeProject
+{
+    partial class CtlModifyPW
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iTalk_Label1 = new iTalk.iTalk_Label();
+            this.iTalk_Label2 = new iTalk.iTalk_Label();
+            this.iTalk_Label3 = new iTalk.iTalk_Label();
+            this.iTalk_Separator2 = new iTalk.iTalk_Separator();
+            this.tboxNowPW = new System.Windows.Forms.TextBox();
+            this.iTalk_Label4 = new iTalk.iTalk_Label();
+            this.iTalk_Separator1 = new iTalk.iTalk_Separator();
+            this.tboxNewPW = new System.Windows.Forms.TextBox();
+            this.iTalk_Label5 = new iTalk.iTalk_Label();
+            this.iTalk_Separator3 = new iTalk.iTalk_Separator();
+            this.tboxNewREPW = new System.Windows.Forms.TextBox();
+            this.iTalk_Label6 = new iTalk.iTalk_Label();
+            this.iTalk_Label7 = new iTalk.iTalk_Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.iTalk_TextBox_Small1 = new iTalk.iTalk_TextBox_Small();
+            this.iTalk_Button_11 = new iTalk.iTalk_Button_1();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // iTalk_Label1
+            // 
+            this.iTalk_Label1.AutoSize = true;
+            this.iTalk_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label1.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.iTalk_Label1.Location = new System.Drawing.Point(67, 56);
+            this.iTalk_Label1.Name = "iTalk_Label1";
+            this.iTalk_Label1.Size = new System.Drawing.Size(254, 13);
+            this.iTalk_Label1.TabIndex = 0;
+            this.iTalk_Label1.Text = "* 다른 아이디/사이트에서 사용한 적 없는 비밀번호";
+            // 
+            // iTalk_Label2
+            // 
+            this.iTalk_Label2.AutoSize = true;
+            this.iTalk_Label2.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label2.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.iTalk_Label2.Location = new System.Drawing.Point(67, 93);
+            this.iTalk_Label2.Name = "iTalk_Label2";
+            this.iTalk_Label2.Size = new System.Drawing.Size(170, 13);
+            this.iTalk_Label2.TabIndex = 1;
+            this.iTalk_Label2.Text = "* 이전에 사용한 적 없는 비밀번호";
+            // 
+            // iTalk_Label3
+            // 
+            this.iTalk_Label3.AutoSize = true;
+            this.iTalk_Label3.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label3.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label3.Location = new System.Drawing.Point(243, 93);
+            this.iTalk_Label3.Name = "iTalk_Label3";
+            this.iTalk_Label3.Size = new System.Drawing.Size(76, 13);
+            this.iTalk_Label3.TabIndex = 2;
+            this.iTalk_Label3.Text = "가 안전합니다";
+            // 
+            // iTalk_Separator2
+            // 
+            this.iTalk_Separator2.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.iTalk_Separator2.Location = new System.Drawing.Point(146, 158);
+            this.iTalk_Separator2.Name = "iTalk_Separator2";
+            this.iTalk_Separator2.Size = new System.Drawing.Size(175, 10);
+            this.iTalk_Separator2.TabIndex = 29;
+            this.iTalk_Separator2.Text = "iTalk_Separator2";
+            // 
+            // tboxNowPW
+            // 
+            this.tboxNowPW.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tboxNowPW.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tboxNowPW.Location = new System.Drawing.Point(146, 141);
+            this.tboxNowPW.Name = "tboxNowPW";
+            this.tboxNowPW.PasswordChar = '*';
+            this.tboxNowPW.Size = new System.Drawing.Size(175, 20);
+            this.tboxNowPW.TabIndex = 28;
+            // 
+            // iTalk_Label4
+            // 
+            this.iTalk_Label4.AutoSize = true;
+            this.iTalk_Label4.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label4.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.iTalk_Label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label4.Location = new System.Drawing.Point(71, 141);
+            this.iTalk_Label4.Name = "iTalk_Label4";
+            this.iTalk_Label4.Size = new System.Drawing.Size(69, 20);
+            this.iTalk_Label4.TabIndex = 27;
+            this.iTalk_Label4.Text = "비밀번호";
+            // 
+            // iTalk_Separator1
+            // 
+            this.iTalk_Separator1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.iTalk_Separator1.Location = new System.Drawing.Point(146, 206);
+            this.iTalk_Separator1.Name = "iTalk_Separator1";
+            this.iTalk_Separator1.Size = new System.Drawing.Size(175, 10);
+            this.iTalk_Separator1.TabIndex = 32;
+            this.iTalk_Separator1.Text = "iTalk_Separator1";
+            // 
+            // tboxNewPW
+            // 
+            this.tboxNewPW.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tboxNewPW.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tboxNewPW.Location = new System.Drawing.Point(146, 189);
+            this.tboxNewPW.Name = "tboxNewPW";
+            this.tboxNewPW.PasswordChar = '*';
+            this.tboxNewPW.Size = new System.Drawing.Size(175, 20);
+            this.tboxNewPW.TabIndex = 31;
+            // 
+            // iTalk_Label5
+            // 
+            this.iTalk_Label5.AutoSize = true;
+            this.iTalk_Label5.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label5.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.iTalk_Label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label5.Location = new System.Drawing.Point(51, 189);
+            this.iTalk_Label5.Name = "iTalk_Label5";
+            this.iTalk_Label5.Size = new System.Drawing.Size(89, 20);
+            this.iTalk_Label5.TabIndex = 30;
+            this.iTalk_Label5.Text = "새 비밀번호";
+            // 
+            // iTalk_Separator3
+            // 
+            this.iTalk_Separator3.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.iTalk_Separator3.Location = new System.Drawing.Point(144, 258);
+            this.iTalk_Separator3.Name = "iTalk_Separator3";
+            this.iTalk_Separator3.Size = new System.Drawing.Size(175, 10);
+            this.iTalk_Separator3.TabIndex = 35;
+            this.iTalk_Separator3.Text = "iTalk_Separator3";
+            // 
+            // tboxNewREPW
+            // 
+            this.tboxNewREPW.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tboxNewREPW.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tboxNewREPW.Location = new System.Drawing.Point(144, 241);
+            this.tboxNewREPW.Name = "tboxNewREPW";
+            this.tboxNewREPW.PasswordChar = '*';
+            this.tboxNewREPW.Size = new System.Drawing.Size(175, 20);
+            this.tboxNewREPW.TabIndex = 34;
+            // 
+            // iTalk_Label6
+            // 
+            this.iTalk_Label6.AutoSize = true;
+            this.iTalk_Label6.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label6.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.iTalk_Label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label6.Location = new System.Drawing.Point(16, 241);
+            this.iTalk_Label6.Name = "iTalk_Label6";
+            this.iTalk_Label6.Size = new System.Drawing.Size(124, 20);
+            this.iTalk_Label6.TabIndex = 33;
+            this.iTalk_Label6.Text = "새 비밀번호 확인";
+            // 
+            // iTalk_Label7
+            // 
+            this.iTalk_Label7.AutoSize = true;
+            this.iTalk_Label7.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label7.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label7.Location = new System.Drawing.Point(95, 299);
+            this.iTalk_Label7.Name = "iTalk_Label7";
+            this.iTalk_Label7.Size = new System.Drawing.Size(209, 13);
+            this.iTalk_Label7.TabIndex = 36;
+            this.iTalk_Label7.Text = "아래 이미지를 보이는 대로 입력해주세요.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(75, 337);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 114);
+            this.pictureBox1.TabIndex = 37;
+            this.pictureBox1.TabStop = false;
+            // 
+            // iTalk_TextBox_Small1
+            // 
+            this.iTalk_TextBox_Small1.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_TextBox_Small1.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.iTalk_TextBox_Small1.ForeColor = System.Drawing.Color.DimGray;
+            this.iTalk_TextBox_Small1.Location = new System.Drawing.Point(98, 467);
+            this.iTalk_TextBox_Small1.MaxLength = 32767;
+            this.iTalk_TextBox_Small1.Multiline = false;
+            this.iTalk_TextBox_Small1.Name = "iTalk_TextBox_Small1";
+            this.iTalk_TextBox_Small1.ReadOnly = false;
+            this.iTalk_TextBox_Small1.Size = new System.Drawing.Size(206, 28);
+            this.iTalk_TextBox_Small1.TabIndex = 38;
+            this.iTalk_TextBox_Small1.Text = "문자를 입력하세요";
+            this.iTalk_TextBox_Small1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            this.iTalk_TextBox_Small1.UseSystemPasswordChar = false;
+            // 
+            // iTalk_Button_11
+            // 
+            this.iTalk_Button_11.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_11.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.iTalk_Button_11.Image = null;
+            this.iTalk_Button_11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_11.Location = new System.Drawing.Point(98, 522);
+            this.iTalk_Button_11.Name = "iTalk_Button_11";
+            this.iTalk_Button_11.Size = new System.Drawing.Size(206, 39);
+            this.iTalk_Button_11.TabIndex = 39;
+            this.iTalk_Button_11.Text = "확인";
+            this.iTalk_Button_11.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // CtlModifyPW
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.iTalk_Button_11);
+            this.Controls.Add(this.iTalk_TextBox_Small1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.iTalk_Label7);
+            this.Controls.Add(this.iTalk_Separator3);
+            this.Controls.Add(this.tboxNewREPW);
+            this.Controls.Add(this.iTalk_Label6);
+            this.Controls.Add(this.iTalk_Separator1);
+            this.Controls.Add(this.tboxNewPW);
+            this.Controls.Add(this.iTalk_Label5);
+            this.Controls.Add(this.iTalk_Separator2);
+            this.Controls.Add(this.tboxNowPW);
+            this.Controls.Add(this.iTalk_Label4);
+            this.Controls.Add(this.iTalk_Label3);
+            this.Controls.Add(this.iTalk_Label2);
+            this.Controls.Add(this.iTalk_Label1);
+            this.Name = "CtlModifyPW";
+            this.Size = new System.Drawing.Size(390, 571);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private iTalk.iTalk_Label iTalk_Label1;
+        private iTalk.iTalk_Label iTalk_Label2;
+        private iTalk.iTalk_Label iTalk_Label3;
+        private iTalk.iTalk_Separator iTalk_Separator2;
+        private System.Windows.Forms.TextBox tboxNowPW;
+        private iTalk.iTalk_Label iTalk_Label4;
+        private iTalk.iTalk_Separator iTalk_Separator1;
+        private System.Windows.Forms.TextBox tboxNewPW;
+        private iTalk.iTalk_Label iTalk_Label5;
+        private iTalk.iTalk_Separator iTalk_Separator3;
+        private System.Windows.Forms.TextBox tboxNewREPW;
+        private iTalk.iTalk_Label iTalk_Label6;
+        private iTalk.iTalk_Label iTalk_Label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private iTalk.iTalk_TextBox_Small iTalk_TextBox_Small1;
+        private iTalk.iTalk_Button_1 iTalk_Button_11;
+    }
+}
---
+++ GoodeeProject/FrmModify.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GoodeeProject
+{
+    partial class FrmModify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMinimum = new iTalk.iTalk_Button_1();
+            this.btnExit = new iTalk.iTalk_Button_1();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSurvey = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnMinimum
+            // 
+            this.btnMinimum.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinimum.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnMinimum.Image = null;
+            this.btnMinimum.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMinimum.Location = new System.Drawing.Point(416, 10);
+            this.btnMinimum.Name = "btnMinimum";
+            this.btnMinimum.Size = new System.Drawing.Size(20, 20);
+            this.btnMinimum.TabIndex = 25;
+            this.btnMinimum.Text = "_";
+            this.btnMinimum.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnExit.Image = null;
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(442, 10);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(20, 20);
+            this.btnExit.TabIndex = 24;
+            this.btnExit.Text = "X";
+            this.btnExit.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(238)))), ((int)(((byte)(233)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.Gray;
+            this.button1.Image = global::GoodeeProject.Properties.Resources.ModifyMember;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(250, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 76);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "회원정보 수정";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnSurvey
+            // 
+            this.btnSurvey.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(238)))), ((int)(((byte)(233)))));
+            this.btnSurvey.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSurvey.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSurvey.ForeColor = System.Drawing.Color.Gray;
+            this.btnSurvey.Image = global::GoodeeProject.Properties.Resources.ModifyPW;
+            this.btnSurvey.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSurvey.Location = new System.Drawing.Point(37, 35);
+            this.btnSurvey.Name = "btnSurvey";
+            this.btnSurvey.Size = new System.Drawing.Size(186, 76);
+            this.btnSurvey.TabIndex = 26;
+            this.btnSurvey.Text = "비밀번호 수정";
+            this.btnSurvey.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSurvey.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(37, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 598);
+            this.panel1.TabIndex = 28;
+            // 
+            // FrmModify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(238)))), ((int)(((byte)(233)))));
+            this.ClientSize = new System.Drawing.Size(472, 778);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSurvey);
+            this.Controls.Add(this.btnMinimum);
+            this.Controls.Add(this.btnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmModify";
+            this.Text = "FrmModify";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private iTalk.iTalk_Button_1 btnMinimum;
+        private iTalk.iTalk_Button_1 btnExit;
+        private System.Windows.Forms.Button btnSurvey;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
