3e53e5d8e0bfe39aa789868199786baca96c6635
cun2744 <cun2744@gmail.com>
2019-02-01 13:17:31 +0900

    2.01 커밋

--- GoodeeProject/FrmCreateID.Designer.cs
+++ GoodeeProject/FrmCreateID.Designer.cs
@@ -38,6 +38,12 @@
             this.iTalk_Separator3 = new iTalk.iTalk_Separator();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.iTalk_Label3 = new iTalk.iTalk_Label();
+            this.iTalk_Separator4 = new iTalk.iTalk_Separator();
+            this.iTalk_Label4 = new iTalk.iTalk_Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.iTalk_Label6 = new iTalk.iTalk_Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // iTalk_Label1
@@ -78,7 +84,7 @@
             // 
             // iTalk_Separator2
             // 
-            this.iTalk_Separator2.Location = new System.Drawing.Point(152, 106);
+            this.iTalk_Separator2.Location = new System.Drawing.Point(152, 108);
             this.iTalk_Separator2.Name = "iTalk_Separator2";
             this.iTalk_Separator2.Size = new System.Drawing.Size(282, 10);
             this.iTalk_Separator2.TabIndex = 52;
@@ -87,14 +93,14 @@
             // tb_Email
             // 
             this.tb_Email.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tb_Email.Location = new System.Drawing.Point(155, 94);
+            this.tb_Email.Location = new System.Drawing.Point(149, 96);
             this.tb_Email.Name = "tb_Email";
-            this.tb_Email.Size = new System.Drawing.Size(279, 14);
+            this.tb_Email.Size = new System.Drawing.Size(285, 14);
             this.tb_Email.TabIndex = 51;
             // 
             // iTalk_Separator1
             // 
-            this.iTalk_Separator1.Location = new System.Drawing.Point(149, 148);
+            this.iTalk_Separator1.Location = new System.Drawing.Point(149, 150);
             this.iTalk_Separator1.Name = "iTalk_Separator1";
             this.iTalk_Separator1.Size = new System.Drawing.Size(282, 10);
             this.iTalk_Separator1.TabIndex = 54;
@@ -103,14 +109,14 @@
             // textBox1
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(152, 136);
+            this.textBox1.Location = new System.Drawing.Point(149, 138);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(279, 14);
+            this.textBox1.Size = new System.Drawing.Size(282, 14);
             this.textBox1.TabIndex = 53;
             // 
             // iTalk_Separator3
             // 
-            this.iTalk_Separator3.Location = new System.Drawing.Point(149, 186);
+            this.iTalk_Separator3.Location = new System.Drawing.Point(149, 188);
             this.iTalk_Separator3.Name = "iTalk_Separator3";
             this.iTalk_Separator3.Size = new System.Drawing.Size(282, 10);
             this.iTalk_Separator3.TabIndex = 57;
@@ -119,9 +125,9 @@
             // textBox2
             // 
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Location = new System.Drawing.Point(152, 174);
+            this.textBox2.Location = new System.Drawing.Point(149, 176);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(279, 14);
+            this.textBox2.Size = new System.Drawing.Size(282, 14);
             this.textBox2.TabIndex = 56;
             // 
             // iTalk_Label3
@@ -132,9 +138,77 @@
             this.iTalk_Label3.ForeColor = System.Drawing.Color.Gray;
             this.iTalk_Label3.Location = new System.Drawing.Point(33, 174);
             this.iTalk_Label3.Name = "iTalk_Label3";
-            this.iTalk_Label3.Size = new System.Drawing.Size(74, 21);
+            this.iTalk_Label3.Size = new System.Drawing.Size(42, 21);
             this.iTalk_Label3.TabIndex = 55;
-            this.iTalk_Label3.Text = "비밀번호";
+            this.iTalk_Label3.Text = "이름";
+            // 
+            // iTalk_Separator4
+            // 
+            this.iTalk_Separator4.Location = new System.Drawing.Point(149, 223);
+            this.iTalk_Separator4.Name = "iTalk_Separator4";
+            this.iTalk_Separator4.Size = new System.Drawing.Size(61, 10);
+            this.iTalk_Separator4.TabIndex = 60;
+            this.iTalk_Separator4.Text = "iTalk_Separator4";
+            // 
+            // iTalk_Label4
+            // 
+            this.iTalk_Label4.AutoSize = true;
+            this.iTalk_Label4.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iTalk_Label4.ForeColor = System.Drawing.Color.Gray;
+            this.iTalk_Label4.Location = new System.Drawing.Point(33, 211);
+            this.iTalk_Label4.Name = "iTalk_Label4";
+            this.iTalk_Label4.Size = new System.Drawing.Size(74, 21);
+            this.iTalk_Label4.TabIndex = 58;
+            this.iTalk_Label4.Text = "생년월일";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.maskedTextBox1.Location = new System.Drawing.Point(149, 211);
+            this.maskedTextBox1.Mask = "0000-00-00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(61, 14);
+            this.maskedTextBox1.TabIndex = 61;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // iTalk_Label6
+            // 
+            this.iTalk_Label6.AutoSize = true;
+            this.iTalk_Label6.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label6.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iTalk_Label6.ForeColor = System.Drawing.Color.Gray;
+            this.iTalk_Label6.Location = new System.Drawing.Point(290, 212);
+            this.iTalk_Label6.Name = "iTalk_Label6";
+            this.iTalk_Label6.Size = new System.Drawing.Size(42, 21);
+            this.iTalk_Label6.TabIndex = 62;
+            this.iTalk_Label6.Text = "성별";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.checkBox1.Location = new System.Drawing.Point(351, 211);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(37, 21);
+            this.checkBox1.TabIndex = 63;
+            this.checkBox1.Text = "남";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox2.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.checkBox2.Location = new System.Drawing.Point(394, 211);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(37, 21);
+            this.checkBox2.TabIndex = 64;
+            this.checkBox2.Text = "여";
+            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // FrmCreateID
             // 
@@ -142,6 +216,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(561, 484);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.iTalk_Label6);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.iTalk_Separator4);
+            this.Controls.Add(this.iTalk_Label4);
             this.Controls.Add(this.iTalk_Separator3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.iTalk_Label3);
@@ -172,5 +252,11 @@
         private iTalk.iTalk_Separator iTalk_Separator3;
         private System.Windows.Forms.TextBox textBox2;
         private iTalk.iTalk_Label iTalk_Label3;
+        private iTalk.iTalk_Separator iTalk_Separator4;
+        private iTalk.iTalk_Label iTalk_Label4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private iTalk.iTalk_Label iTalk_Label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
